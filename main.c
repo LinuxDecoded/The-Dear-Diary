94bf0ed9b62dc37fb436b86529805f99648e03cc
Ajay Sharma <ram1703sharma@gmail.com>
2022-03-08 12:16:46 +0530

    final_changes

--- main.c
+++ main.c
@@ -8,9 +8,6 @@
 int main() {
 
     SetConsoleTitle("The Dear Diary");
-    HWND c_window = GetConsoleWindow();
-    HMENU c_menu = GetSystemMenu(c_window, FALSE);
-    EnableMenuItem(c_menu, SC_CLOSE, MF_GRAYED);
 
     start_screen();
     
